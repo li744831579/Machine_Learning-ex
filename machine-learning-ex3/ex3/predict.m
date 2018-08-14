6d7f5c2e8e2371121769a24a68fadfe8ad7b01c4
li744831579 <jun0213@163.com>
2018-08-14 22:20:48 +0800

    jun0213@163.com
    add predict

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -20,7 +20,12 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+X = [ones(m,1) X];
 
+ a2 = sigmoid(X * Theta1');
+ a3 = sigmoid([ones(size(a2,1),1) a2] * Theta2');
+ [value,p] = max(a3,[],2);
+ 
 
 
 
